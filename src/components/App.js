7f96ff1a0762818cd65fc05e8f21b2484445d2cc
Christos Paschalidis <paschalidi@outlook.com>
2019-09-27 17:57:43 +0200

    adds all of it at once

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+import '../styles/App.css';
+import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
+import Header from './Header'
+import Login from './Login'
+
+function App() {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </div>
+  )
+}
+
+export default App;
